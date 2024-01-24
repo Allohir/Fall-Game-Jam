9f2e7791452c2eae946ba2289905b3bf01d4c930
Allohir <123628325+Allohir@users.noreply.github.com>
2024-01-24 19:56:04 +0400

    Add files via upload

---
+++ TextTypingEffect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class TextTypingEffect : MonoBehaviour
+{
+    [SerializeField]
+    private float typingSpeed = 0.03f;
+    private string fullText;
+    private string currentText = "";
+
+    void Start()
+    {
+        fullText = this.GetComponent<TMP_Text>().text;
+        GetComponent<TMP_Text>().text = "";
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            GetComponent<TMP_Text>().text = currentText;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
